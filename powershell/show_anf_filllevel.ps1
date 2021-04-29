b7552b62257fa7621ba00a94a6d0ef1fe26bf3aa
Donovan Hughes <dohughes@microsoft.com>
2021-04-29 08:41:44 +0200

    Add export feature to ANF fill level

--- powershell/show_anf_filllevel.ps1
+++ powershell/show_anf_filllevel.ps1
@@ -2,17 +2,21 @@
 .SYNOPSIS
     Display a list of Azure NetApp Files volumes and their current filling level
 .EXAMPLE
-    PS C:\> show_anf_filllevel.ps1
+    PS C:\> show_anf_filllevel.ps1 [-export output.csv]
 .INPUTS
-    The script will find volumes using an Azure Resource Graph query for the current user
+    Parameter -export can be used to write the output to a CSV file instead of on-screen.
 .OUTPUTS
-    Display a list of account/pool/volume and current filling level
+    Display a list of account/pool/volume, volume size and current filling level
 .NOTES
     This script requires the Az module Az.ResourceGraph. You can install it with:
     Install-Module -Name Az.ResourceGraph
 #>
 
-$netAppVolumes = Search-AzGraph -Query "Resources | where type == 'microsoft.netapp/netappaccounts/capacitypools/volumes' | project name, id | order by name"
+param (
+    [string]$export
+)
+
+$netAppVolumes = Search-AzGraph -Query "Resources | where type == 'microsoft.netapp/netappaccounts/capacitypools/volumes' | project name, id, size=properties.usageThreshold, tier=properties.serviceLevel | order by name"
 
 $endTime = Get-Date
 $startTime = $endTime.AddMinutes(-5)
@@ -21,8 +25,12 @@ $WarningPreference = "SilentlyContinue"
 
 foreach($netAppVolume in $netAppVolumes) {
     $fillLevel = (Get-AzMetric -ResourceId $netAppVolume.id -MetricName VolumeConsumedSizePercentage -TimeGrain 00:05:00 -StartTime $startTime -EndTime $endTime).Data.Average
-    $resultRow = [PSCustomObject]@{"Account/Pool/Volume"=$netAppVolume.name;"Fill %"=[math]::Round($fillLevel,2)}
+    $resultRow = [PSCustomObject]@{"Account/Pool/Volume"=$netAppVolume.name;"Tier"=$netAppVolume.tier;"Fill %"=[math]::Round($fillLevel,2);"Size"=$netAppVolume.size}
     $resultTable.Add($resultRow) | Out-Null
 }
 
-$resultTable
+if ($PSBoundParameters.ContainsKey('export')) {
+    $resultTable | Export-Csv -Path .\$export
+} else {
+    $resultTable | Format-Table -AutoSize
+}

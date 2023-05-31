0fb26dd30cdf13f147fdcdf28accdf96a890cccf
Donovan Hughes <dohughes@microsoft.com>
2023-05-31 10:47:25 +0200

    .

--- bash/acs/send_sms.sh
+++ bash/acs/send_sms.sh
@@ -9,6 +9,6 @@
 senderID="Contoso"                  # Up to 11 characters
 recipient="+46000000000"            # Phone number in E.164 format
 message="Hello World SMS Test"      # SMS message content
-connString="endpoint=https://acsdemo1.communication.azure.com/;accesskey=dfTEsuCLrMPTJvameKhLRMAsmJ2VaejCuKI3f8VLlqBxZYfmYASTcbNt6ydaEh58bcq9xqQM3Ppi86dZZ5DRWg=="
+connString="endpoint=https://XXXXXXXXX.communication.azure.com/;accesskey=YYYYYYYYYYYYYY"
 
 az communication sms send --sender "$senderID" --recipient $recipient --message "$message" --connection-string $connString

e57830a1c5aa7a38306cdf764d9bfa7ff874b159
Donovan Hughes <dohughes@microsoft.com>
2023-07-06 17:01:51 +0200

    New script to retrieve resource cost history

---
+++ powershell/get_cost_history.ps1
@@ -0,0 +1,116 @@
+<#
+.SYNOPSIS
+    Take a given resource ID and return the cost history for that resource. For this we use the Microsoft.CostManagement provider
+    of the resource group containing the resource.
+    Requires Az.CostManagement module
+
+.PARAMETER ParameterName
+    $resourceID : The resource ID of the resource to be examined (mandatory)
+    $startDate : The start date of the period to be examined (default is the first day of the month, 6 months ago)
+    $endDate : The end date of the period to be examined (default is the last day of the previous month)
+
+.INPUTS
+    None
+
+.OUTPUTS
+    A table showing the cost history of the given resource over the requested period
+
+.EXAMPLE
+    .\get_cost_history.ps1 -resourceId "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourcegroups/xxxxxxxx/providers/microsoft.compute/disks/xxxxxxx" -startDate "2023-01-01" -endDate "2023-06-30"
+
+.NOTES
+
+#>
+
+param (
+    [Parameter(Mandatory=$true)][string]$resourceId,
+    [string]$startDate,
+    [string]$endDate
+)
+
+$startDate = (Get-Date).AddMonths(-6).ToString("yyyy-MM-01")                      # the first day of the month 6 months ago
+$endDate = (Get-Date).AddDays(-1 * (Get-Date).Day).ToString("yyyy-MM-dd")         # the last day of the previous month
+
+$timeframe = "Custom"            # Supported types are BillingMonthToDate, Custom, MonthToDate, TheLastBillingMonth, TheLastMonth, WeekToDate
+$granularity = "Monthly"         # Supported types are Daily and Monthly so far. Omit just to get the total cost.
+$type = "AmortizedCost"          # Supported types are Usage (deprecated), ActualCost, and AmortizedCost
+# https://stackoverflow.com/questions/68223909/in-the-azure-consumption-usage-details-api-what-is-the-difference-between-the-m
+
+$grouping = @(
+    @{
+        type = "Dimension"
+        name = "ResourceId"
+    }
+)
+
+<# Valid dimensions for grouping are:
+AccountName
+BenefitId
+BenefitName
+BillingAccountId
+BillingMonth
+BillingPeriod
+ChargeType
+ConsumedService
+CostAllocationRuleName
+DepartmentName
+EnrollmentAccountName
+Frequency
+InvoiceNumber
+MarkupRuleName
+Meter
+MeterCategory
+MeterId
+MeterSubcategory
+PartNumber
+PricingModel
+PublisherType
+ReservationId
+ReservationName
+ResourceGroup
+ResourceGroupName
+ResourceGuid
+ResourceId
+ResourceLocation
+ResourceType
+ServiceName
+ServiceTier
+SubscriptionId
+SubscriptionName
+#>
+
+$aggregation = @{
+    PreTaxCost = @{
+        type = "Sum"
+        name = "PreTaxCost"
+    }
+}
+
+# For the purposes of this script we will set the scope to be the resource group, which we can extract from the resource ID.
+$resourceId.Split("/")[1..4] | ForEach {$scope += "/$_"}
+
+$dimensions = New-AzCostManagementQueryComparisonExpressionObject -Name 'ResourceId' -Value $resourceId -Operator 'In'
+$filter = New-AzCostManagementQueryFilterObject -Dimensions $dimensions
+$queryResult = Invoke-AzCostManagementQuery `
+    -Scope $scope `
+    -Timeframe $timeframe `
+    -Type $type `
+    -DatasetFilter $filter `
+    -TimePeriodFrom $startDate `
+    -TimePeriodTo $endDate `
+    -DatasetGrouping $grouping `
+    -DatasetAggregation $aggregation `
+    -DatasetGranularity $granularity
+#-Debug
+
+# Convert the query result into a table
+$table = @()
+for ($i = 0; $i -lt $queryResult.Row.Count; $i++) {
+    $row = [PSCustomObject]@{}
+    for ($j = 0; $j -lt $queryResult.Column.Count; $j++) {
+        $row | Add-Member -MemberType NoteProperty -Name $queryResult.Column.Name[$j] -Value $queryResult.Row[$i][$j]
+    }
+    $table += $row
+}
+
+$table | Format-Table -AutoSize

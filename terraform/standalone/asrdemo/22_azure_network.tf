f960ce123ae0e8261ab695c9e05acdee5fa0c04f
Donovan Hughes <dohughes@microsoft.com>
2023-07-25 14:53:09 +0000

    .

--- terraform/standalone/asrdemo/22_azure_network.tf
+++ terraform/standalone/asrdemo/22_azure_network.tf
@@ -12,7 +12,7 @@ resource "azurerm_subnet" "azure-workload" {
   name                 = var.azure_subnet_name
   resource_group_name  = azurerm_resource_group.azure.name
   virtual_network_name = azurerm_virtual_network.azure.name
-  address_prefixes     = ["192.168.0.0/24"]
+  address_prefixes     = ["192.168.1.0/24"]
 }
 
 resource "azurerm_subnet" "azure-bastion" {
@@ -27,14 +27,15 @@ resource "azurerm_virtual_network" "azure-test" {
   name                = var.azure_test_vnet_name
   location            = azurerm_resource_group.azure.location
   resource_group_name = azurerm_resource_group.azure.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["192.168.0.0/16"]
+  dns_servers         = ["192.168.1.10"]
 }
 
 resource "azurerm_subnet" "azure-test-workload" {
   name                 = var.azure_subnet_name
   resource_group_name  = azurerm_resource_group.azure.name
   virtual_network_name = azurerm_virtual_network.azure-test.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["192.168.1.0/24"]
 }
 
 resource "azurerm_subnet" "azure-test-bastion" {
@@ -42,7 +43,7 @@ resource "azurerm_subnet" "azure-test-bastion" {
   name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.azure.name
   virtual_network_name = azurerm_virtual_network.azure-test.name
-  address_prefixes     = ["10.0.10.0/24"]
+  address_prefixes     = ["192.168.10.0/24"]
 }
 
 resource "azurerm_network_security_group" "azure" {

1dc0159d554db4db41bcdbda4b4f4b88b41e9352
Donovan Hughes <dohughes@microsoft.com>
2023-08-22 14:00:51 +0200

    Add group label

--- terraform/standalone/asrdemo/20_azure_resourcegroup.tf
+++ terraform/standalone/asrdemo/20_azure_resourcegroup.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "azure" {
-  name     = var.azure_resource_group_name-var.group_label
+  name     = "${var.azure_resource_group_name}-${var.group_label}"
   location = var.location
 }

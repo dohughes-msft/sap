f08b7faa41bc0ee14ff9758cec82ad7f4684568b
Donovan Hughes <dohughes@microsoft.com>
2023-08-23 14:56:35 +0200

    Add admin_user variable

--- terraform/standalone/asrdemo/00_global_vars.tf
+++ terraform/standalone/asrdemo/00_global_vars.tf
@@ -8,6 +8,11 @@ variable "onprem_resource_group_name" {
   description = "Resource group name that will hold the simulated on-premises resources"
 }
 
+variable "admin_user" {
+  type = string
+  description = "The administrator username for the on-premises Hyper-V host"
+}
+
 # Do not put this in .tfvars - provide it on the command line or export it as an
 # environment variable before starting Terraform
 # For example: 
@@ -18,6 +23,11 @@ variable "admin_password" {
   type = string
   sensitive = true
   description = "The administrator password for the on-premises Hyper-V host"
+}
+
+variable "admin_ip_address" {
+  type = string
+  description = "The IP address or range from which access to the on-premises Hyper-V host is allowed, if not using Bastion"
 }
 
 variable "group_label" {
@@ -48,11 +58,6 @@ variable "hyperv_hostname" {
 variable "hyperv_host_size" {
   type = string
   description = "The size (Azure SKU) of the on-premises Hyper-V host"
-}
-
-variable "admin_ip_address" {
-  type = string
-  description = "The IP address or range from which access to the on-premises Hyper-V host is allowed, if not using Bastion"
 }
 
 variable "use_bastion" {
